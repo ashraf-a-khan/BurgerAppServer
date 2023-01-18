4355cb699c1c4f0e6937f2771879ebca21d9bc8b
ashraf-a-khan <khan.ashraf2311@gmail.com>
2023-01-18 02:31:12 -0500

    change cookie destroy code

--- controllers/user.js
+++ controllers/user.js
@@ -27,8 +27,8 @@ export const logout = (req, res, next) => {
     req.session.destroy((err) => {
         if (err) return next(err);
         res.clearCookie("connect.sid", {
-            secure: process.env.NODE_ENV === "production",
-            httpOnly: process.env.NODE_ENV === "production",
+            secure: process.env.NODE_ENV === "development" ? false : true,
+            httpOnly: process.env.NODE_ENV === "development" ? false : true,
             sameSite: process.env.NODE_ENV === "development" ? false : "none",
         });
         res.status(200).json({
