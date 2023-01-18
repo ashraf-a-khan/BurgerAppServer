98eae6cd9d7095ee579afa1fb706a57da2b27d4f
ashraf-a-khan <khan.ashraf2311@gmail.com>
2023-01-18 18:00:44 -0500

    testing destroy

--- controllers/user.js
+++ controllers/user.js
@@ -21,21 +21,26 @@ export const logout = (req, res, next) => {
     //         message: "Logged Out",
     //     });
     // });
-    req.session.destroy((err) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send("Error logging out");
-        } else {
-            res.clearCookie("connect.sid", {
-                // secure: process.env.NODE_ENV === "development" ? false : true,
-                // httpOnly: process.env.NODE_ENV === "development" ? false : true,
-                // sameSite:
-                //     process.env.NODE_ENV === "development" ? false : "none",
-                path: "/",
-            });
-            res.status(200).send("Successfully logged out");
-        }
-    });
+
+    req.session.destroy();
+    res.clearCookie("connect.sid"); // clean up!
+    return res.json({ msg: "logging you out" });
+
+    // req.session.destroy((err) => {
+    //     if (err) {
+    //         console.error(err);
+    //         res.status(500).send("Error logging out");
+    //     } else {
+    //         res.clearCookie("connect.sid", {
+    //             // secure: process.env.NODE_ENV === "development" ? false : true,
+    //             // httpOnly: process.env.NODE_ENV === "development" ? false : true,
+    //             // sameSite:
+    //             //     process.env.NODE_ENV === "development" ? false : "none",
+    //             path: "/",
+    //         });
+    //         res.status(200).send("Successfully logged out");
+    //     }
+    // });
 };
 
 export const getAdminUsers = asyncError(async (req, res, next) => {
