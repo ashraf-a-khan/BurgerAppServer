a862853be249716843d56e28efda3c94fd0593ba
ashraf-a-khan <khan.ashraf2311@gmail.com>
2023-01-18 21:31:29 -0500

    Print token

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ import { User } from "../models/User.js";
 import { Order } from "../models/Order.js";
 
 export const myProfile = (req, res, next) => {
-    console.log("res", res);
+    // console.log("res", res);
     res.status(200).json({
         success: true,
         user: req.user,
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ import ErrorHandler from "../utils/ErrorHandler.js";
 
 export const isAuthenticated = (req, res, next) => {
     const token = req.cookies["connect.sid"];
-
+    console.log("token", token);
     if (!token) {
         return next(new ErrorHandler("Not Logged In", 401));
     }
